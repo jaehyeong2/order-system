2adb4591903722804473df56653eab139327946e
jaehyeong <wogud1514@gmail.com>
2024-09-22 11:31:53 +0900

    chore: apache httpClient

--- oms-api/build.gradle.kts
+++ oms-api/build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.3.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
